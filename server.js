e27bb6972ca53a17e3561afa060bb4701cba9aad
crystal025 <oj3339@naver.com>
2022-12-18 20:16:30 +0900

    request의 body 값 받아오기

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("hello world!");
@@ -15,4 +17,6 @@ app.get("/write", (require, response) => {
 
 app.post("/add", (req, res) => {
   res.send("전송 완료");
+  console.log(req.body.content);
+  console.log(req.body.date);
 });
